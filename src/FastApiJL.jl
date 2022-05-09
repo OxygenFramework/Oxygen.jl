fa5260ff4cb5445c3cdcb0111c299b9f347b28ee
Nathan Ortega <nate.ortega95@gmail.com>
2022-05-09 11:47:15 -0400

    refactored register() and moved functions

--- src/FastApiJL.jl
+++ src/FastApiJL.jl
@@ -6,6 +6,26 @@ module FastApiJL
     # define REST endpoints to dispatch to "service" functions
     const ROUTER = HTTP.Router()
 
+    function start(port=8081)
+        HTTP.serve(JSONHandler, Sockets.localhost, port)
+    end
+
+    function start(customHandler::Function, port=8081)
+        HTTP.serve(req -> customHandler(req), Sockets.localhost, port)
+    end
+
+    macro get(path, func)
+        quote 
+            @register "GET" $path $(esc(func))
+        end
+    end
+
+    macro post(path, func)
+        quote 
+            @register "POST" $path $(esc(func))
+        end
+    end
+
     function JSONHandler(req::HTTP.Request)
         # first check if there's any request body
         body = IOBuffer(HTTP.payload(req))
@@ -19,25 +39,8 @@ module FastApiJL
         return HTTP.Response(200, JSON3.write(response_body))
     end
 
-    macro get(path, func)
-        quote 
-            @registerwithparams "GET" $path $(esc(func))
-        end
-    end
-
-    macro post(path, func)
-        quote 
-            @registerwithparams "POST" $path $(esc(func))
-        end
-    end
-
     macro register(method, path, func)
-        quote 
-            HTTP.@register(ROUTER, $method, $path, $(esc(func)))
-        end
-    end
 
-    macro registerwithparams(method, path, func)
         # determine if we have parameters defined in our path
         local hasParams = contains(path, ":")
         local cleanpath = hasParams ? replace(path, r":[a-z]+" => "*") : path 
@@ -46,8 +49,8 @@ module FastApiJL
         local pattern = HTTP.URIs.splitpath(path)
         local paramPositions = Dict(index => value for (index, value) in enumerate(pattern) if contains(value, ":")) 
         
-        quote 
-            @register $method $cleanpath function (req)
+        local handleRequest = quote 
+            function (req)
                 if $hasParams
                     local splitPath = enumerate(HTTP.URIs.splitpath(req.target))
                     local params = Dict($paramPositions[index] => value for (index, value) in splitPath if haskey($paramPositions, index))
@@ -59,15 +62,10 @@ module FastApiJL
                 end
             end
         end
-    end
 
-
-    function start(port=8081)
-        HTTP.serve(JSONHandler, Sockets.localhost, port)
-    end
-
-    function start(customHandler::Function, port=8081)
-        HTTP.serve(req -> customHandler(req), Sockets.localhost, port)
+        quote 
+            HTTP.@register(ROUTER, $method, $cleanpath, $handleRequest)
+        end
     end
 
 end

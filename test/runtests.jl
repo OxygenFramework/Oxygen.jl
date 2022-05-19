87a18432ccbeb03fc9967f23d0168e862873a7dc
Nathan Ortega <nate.ortega95@gmail.com>
2022-05-19 09:58:43 -0400

    added helper functions to unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,18 +30,35 @@ module RunTests
         return Person("joe", 20)
     end 
     
+
+    ### Helper functions used to parse the body of each response
+
+    function string(response::HTTP.Response)
+        return String(response.body)
+    end
+
+    function json(response::HTTP.Response)
+        return JSON3.read(String(response.body))
+    end
+
+    function json(response::HTTP.Response, classtype)
+        return JSON3.read(String(response.body), classtype)
+    end
+
+    ###
+
     r = internalrequest(HTTP.Request("GET", "/test"))
     @test r.status == 200
-    @test String(r.body) == "hello world!"
+    @test string(r) == "hello world!"
 
     r = internalrequest(HTTP.Request("GET", "/multiply/5/8"))
     @test r.status == 200
-    @test String(r.body) == "40.0"
+    @test string(r) == "40.0"
 
     r = internalrequest(HTTP.Request("GET", "/multiply/a/8"), true)
     @test r.status == 500
 
     r = internalrequest(HTTP.Request("GET", "/json"))
     @test r.status == 200
-    @test JSON3.read(String(r.body), Person) == Person("joe", 20)
+    @test json(r, Person)== Person("joe", 20)
 end 

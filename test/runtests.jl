0a37385a6972647d083a6c1e218838595a505f2b
Nathan Ortega <nate.ortega95@gmail.com>
2022-05-19 21:41:57 -0400

    added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,9 @@ module RunTests
 
     StructTypes.StructType(::Type{Person}) = StructTypes.Struct()
 
+    # mount all files inside the content folder
+    @staticfiles "content"
+
     @get "/anonymous" function()
         return "no args"
     end
@@ -29,6 +32,41 @@ module RunTests
     @get "/json" function(req)
         return Person("joe", 20)
     end 
+
+    @get "/html" function(req)
+        return html(""" 
+            <!DOCTYPE html>
+                <html>
+                <body> 
+                    <h1>hello world</h1>
+                </body>
+            </html>
+        """)
+    end 
+
+    @route "/get" ["GET"] function(req)
+        return "get"
+    end 
+
+    @get "/query" function(req)
+        return queryparams(req)
+    end 
+
+    @post "/post" function(req)
+        return "post"
+    end 
+
+    @put "/put" function(req)
+        return "put"
+    end 
+
+    @patch "/patch" function(req)
+        return "patch"
+    end 
+
+    @delete "/delete" function(req)
+        return "delete"
+    end 
     
     r = internalrequest(HTTP.Request("GET", "/test"))
     @test r.status == 200
@@ -43,5 +81,57 @@ module RunTests
 
     r = internalrequest(HTTP.Request("GET", "/json"))
     @test r.status == 200
-    @test json(r, Person)== Person("joe", 20)
+    @test json(r, Person) == Person("joe", 20)
+
+    r = internalrequest(HTTP.Request("GET", "/html"))
+    @test r.status == 200
+    @test Dict(r.headers)["Content-Type"] == "text/html; charset=utf-8"
+
+
+    # GET, PUT, POST, PATCH, DELETE, route macro tests 
+
+    r = internalrequest(HTTP.Request("GET", "/get"))
+    @test r.status == 200
+    @test text(r) == "get"
+
+    r = internalrequest(HTTP.Request("POST", "/post"))
+    @test r.status == 200
+    @test text(r) == "post"
+
+    r = internalrequest(HTTP.Request("PUT", "/put"))
+    @test r.status == 200
+    @test text(r) == "put"
+
+    r = internalrequest(HTTP.Request("PATCH", "/patch"))
+    @test r.status == 200
+    @test text(r) == "patch"
+
+
+    # Query params tests 
+
+    r = internalrequest(HTTP.Request("GET", "/query?message=hello"))
+    @test r.status == 200
+    @test json(r)["message"] == "hello"
+
+    r = internalrequest(HTTP.Request("GET", "/query?message=hello&value=5"))
+    data = json(r)
+    @test r.status == 200
+    @test data["message"] == "hello"
+    @test data["value"] == "5"
+
+
+    # Get mounted static files
+
+    r = internalrequest(HTTP.Request("GET", "/static/content/test.txt"))
+    body = text(r)
+    @test r.status == 200
+    @test Dict(r.headers)["Content-Type"] == "text/plain; charset=utf-8"
+    @test body == file("content/test.txt")
+    @test body == "this is a sample text file"
+
+    r = internalrequest(HTTP.Request("GET", "/static/content/sample.html"))
+    @test r.status == 200
+    @test Dict(r.headers)["Content-Type"] == "text/html; charset=utf-8"
+    @test text(r) == file("content/sample.html")
+
 end 

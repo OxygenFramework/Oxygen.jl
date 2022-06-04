0923dec339bfe50bf43f1e5017120a8960d29da4
Nathan Ortega <nate.ortega95@gmail.com>
2022-06-04 12:40:12 -0400

    moved doc-generation code into docgen.jl file

--- docs/make.jl
+++ docs/make.jl
@@ -1,34 +1,6 @@
 using Documenter
 using Oxygen
 
-# escape and replace text as needed
-function sanitizefile(filelines)
-    content = ""
-    skips = 0
-    for line in filelines    
-        if skips > 0
-            skips -= 1
-            continue
-        elseif contains(line, "<!-- START HTML -->")
-            content *= "```@raw html\n"
-        elseif contains(line, "<!-- END HTML -->")
-            content *= "```\n"
-        elseif contains(line, "<!-- REPLACE")        
-            _, replace_value, _ = split(line, r"\{\{|\}\}")
-            content *= "$replace_value\n"
-            skips += 1 
-        else 
-            content *= "$line\n"
-        end
-    end
-    return content
-end
-
-# generate index.md and move assets
-readme = readlines(open("../README.md"))
-write("src/index.md", sanitizefile(readme))
-cp("../oxygen.png", "./src/oxygen.png", force=true)
-
 makedocs(
     sitename = "Oxygen.jl",
     format = Documenter.HTML(),

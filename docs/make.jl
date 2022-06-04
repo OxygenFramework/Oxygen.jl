5f38363dbf74447bb839838eca1ade2586ddf659
Nathan Ortega <nate.ortega95@gmail.com>
2022-06-04 14:13:29 -0400

    changed devbranch develop to retrigger docs build

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,8 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/ndortega/Oxygen.jl.git"
+    repo = "github.com/ndortega/Oxygen.jl.git",
+    devbranch = "develop"
 )
 
 

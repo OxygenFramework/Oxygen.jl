5c234fa47f3ac310f0eb7487fdf487c230ecec4c
Nathan Ortega <nate.ortega95@gmail.com>
2022-07-11 23:23:28 -0400

    removed unused function

--- demo/routerdemo.jl
+++ demo/routerdemo.jl
@@ -10,10 +10,6 @@ repeat = router("/repeat", interval = 1, tags=["repeat"])
 hellorouter = router("/hello", tags=["greeting"])
 emptyrouter = router()
 
-function blah()
-    return "/test/"
-end
-
 @get router("/spam", tags=["spam"], interval=0.25) function()
     println("spam")
 end

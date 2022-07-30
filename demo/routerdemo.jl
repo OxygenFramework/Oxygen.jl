56bde55da74abeda2ce534eea5fdf481c6c8ac55
Nathan Ortega <nate.ortega95@gmail.com>
2022-07-30 12:14:39 -0400

    organized the RouterDemo

--- demo/routerdemo.jl
+++ demo/routerdemo.jl
@@ -3,33 +3,35 @@ module RouterDemo
 include("../src/Oxygen.jl")
 using .Oxygen
 using HTTP
-using StructTypes
 using JSON3
 
-repeat = router("/repeat", interval = 1, tags=["repeat"])
 hellorouter = router("/hello", tags=["greeting"])
+
+@get hellorouter("/excited", tags=["good"]) function(req)
+    return "excited"
+end
+
+@get hellorouter("/sad") function(req)
+    return "sad"
+end
+
+repeat = router("/repeat", interval = 1, tags=["repeat"])
+
+@get repeat("/one") function(req)
+    println("one")
+    return "one"
+end
+
 emptyrouter = router()
-
-@get router("/spam", tags=["spam"], interval=0.25) function()
-    println("spam")
-end
-
-@get hellorouter("/nice", tags=["good"]) function(req)
-    return "nice"
-end
-
-@get hellorouter("/other") function(req)
-    return "other"
-end
 
 @get emptyrouter("/empty", interval = 3) function(req)
     println("empty")
     return "empty"
 end
 
-@get repeat("/one") function(req)
-    println("one")
-    return "one"
+# you can also pass the `router()` function itself
+@get router("/spam", tags=["spam"], interval=0.25) function()
+    println("spam")
 end
 
 serve()

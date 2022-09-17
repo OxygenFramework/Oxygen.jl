5843bfb1708cad18be5d91ebb28ef4525f3542d1
Nathan Ortega <nate.ortega95@gmail.com>
2022-09-17 16:03:44 -0400

    can now support week & month abbreviations

--- src/cron.jl
+++ src/cron.jl
@@ -18,51 +18,95 @@ function everysecond(func)
 end
 
 
+weeknames = Dict(
+    "SUN" => 0,
+    "MON" => 1,
+    "TUE" => 2,
+    "WED" => 3,
+    "THU" => 4,
+    "FRI" => 5,
+    "SAT" => 6,
+)
+
+monthnames = Dict(
+    "JAN" => 1,
+    "FEB" => 2,
+    "MAR" => 3,
+    "APR" => 4,
+    "MAY" => 5,
+    "JUN" => 6,
+    "JUL" => 7,
+    "AUG" => 8,
+    "SEP" => 9,
+    "OCT" => 10,
+    "NOV" => 11,
+    "DEC" => 12
+)
+
+function translate(input::SubString)
+    if haskey(weeknames, input)
+        return weeknames[input]
+    elseif haskey(monthnames, input)
+        return weeknames[input]
+    else 
+        return input
+    end
+end
+
+
+function customparse(type, input)
+    if isa(input, type)
+        return input
+    end 
+    return parse(type, input)
+end
 
 # https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/scheduling/support/CronExpression.html
-# range expressions: 8-10
-# list expressions: 6,19
 # https://crontab.cronhub.io/
 
 function matchPrimitives(input::SubString, time::DateTime, converter) :: Bool
     current = converter(time)
-    numericvalue = tryparse(Int8, input)
+    numericvalue = tryparse(Int64, input)
 
     # Every Second
     if input == "*"
         return true 
+
     # At X seconds past the minute
     elseif numericvalue !== nothing
         return numericvalue == current
 
     elseif contains(input, ",")
         lowerbound, upperbound = split(input, ",")
-        return current == parse(Int8, lowerbound) || current === parse(Int8, upperbound)
+        lowerbound, upperbound = translate(lowerbound), translate(upperbound)
+        return current == customparse(Int64, lowerbound) || current === customparse(Int64, upperbound)
 
     elseif contains(input, "-")
         lowerbound, upperbound = split(input, "-")
+        lowerbound, upperbound = translate(lowerbound), translate(upperbound)
         if lowerbound == "*"
-            return current <= parse(Int8, upperbound)
+            return current <= customparse(Int64, upperbound)
         elseif upperbound == "*"
-            return current >= parse(Int8, lowerbound)
+            return current >= customparse(Int64, lowerbound)
         else 
-            return current >= parse(Int8, lowerbound) && current <= parse(Int8, upperbound)
+            return current >= customparse(Int64, lowerbound) && current <= customparse(Int64, upperbound)
         end
         
     elseif contains(input, "/")
         numerator, denominator = split(input, "/")
+        numerator, denominator = translate(numerator), translate(denominator)
         # Every second, starting at Y seconds past the minute
         if denominator == "*"
-            numerator = parse(Int8, numerator)
+            numerator = customparse(Int64, numerator)
             return current >= numerator
         elseif numerator == "*"
             # Every X seconds
-            denominator = parse(Int8, denominator)
+            denominator = customparse(Int64, denominator)
             return current % denominator == 0
         else
             # Every X seconds, starting at Y seconds past the minute
-            numerator = parse(Int8, numerator)
-            denominator = parse(Int8, denominator)
+            numerator = customparse(Int64, numerator)
+            denominator = customparse(Int64, denominator)
             return current % denominator == 0 && current >= numerator
         end
     else 
@@ -70,8 +114,7 @@ function matchPrimitives(input::SubString, time::DateTime, converter) :: Bool
     end
 end
 
-cron = "* 34-35 23 * * *" # every 10 seconds
-
+cron = "* 34-35 23 * * MON-WED" # every 10 seconds
 function run(time:: DateTime)
     seconds_expression, minute_expression, hour_expression,
     dayofmonth_expression, month_expression, dayofweek_expression = split(cron, " ")

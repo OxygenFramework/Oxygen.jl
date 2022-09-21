bd3429b476bd5ee3e0304ae3a133bab435498891
Nathan Ortega <nate.ortega95@gmail.com>
2022-09-20 22:00:53 -0400

    added doc strings to example

--- demo/crondemo.jl
+++ demo/crondemo.jl
@@ -6,10 +6,12 @@ using HTTP
 
 value = 0
 
+# Here's utility macro to call functions directly
 @cron "*/5" function()
     println("every 5 seconds")
 end
 
+# You can also just use the 'cron' keyword that's apart of the router() function
 @get router("/increment", cron="*/3") function()
     global value += 1
     println(value)

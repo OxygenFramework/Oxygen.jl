47c228f20654642bd5e1f71a2973d3bce0d5da22
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-01-16 00:49:40 +0530

    removing redundant method definition and using from base (#83)

--- src/util.jl
+++ src/util.jl
@@ -3,18 +3,11 @@ using HTTP
 using JSON3
 using Dates
 
-export method_argnames, countargs, recursive_merge, parseparam, queryparams, html, redirect
+export countargs, recursive_merge, parseparam, queryparams, html, redirect
 
 # return the number of args inside a function
 function countargs(f::Function)
     return methods(f) |> first |> x -> x.nargs
-end
-
-# https://discourse.julialang.org/t/get-the-argument-names-of-an-function/32902/4
-function method_argnames(m::Method)
-    argnames = ccall(:jl_uncompress_argnames, Vector{Symbol}, (Any,), m.slot_syms)
-    isempty(argnames) && return argnames
-    return argnames[1:m.nargs]
 end
 
 # https://discourse.julialang.org/t/multi-layer-dict-merge/27261/7

014e94b7b90ba28112b18908fdbc9ce1a6d7ba15
Nathan Ortega <nate.ortega95@gmail.com>
2023-04-18 21:14:02 -0400

    1.) added the ability to set the mount dir to the route "/" 2.) converted several internal macros to functions 3.) created corresponding staticfiles & dynamicfiles functions that perform the same as the macros

--- demo/main.jl
+++ demo/main.jl
@@ -14,110 +14,113 @@ end
 # Add a supporting struct type definition to the Animal struct
 StructTypes.StructType(::Type{Animal}) = StructTypes.Struct()
 
-@get "/" function()
-    return "home"
-end
+# @get "/" function()
+#     return "home"
+# end
 
-@get "/killserver" function ()
-    terminate()
-end
+# @get "/killserver" function ()
+#     terminate()
+# end
 
-# add a default handler for unmatched requests
-@get "*" function () 
-    return "looks like you hit an endpoint that doesn't exist"
-end
+# # add a default handler for unmatched requests
+# @get "*" function () 
+#     return "looks like you hit an endpoint that doesn't exist"
+# end
 
-# You can also interpolate variables into the endpoint
-operations = Dict("add" => +, "multiply" => *)
-for (pathname, operator) in operations
-    @get "/$pathname/{a}/{b}" function (req, a::Float64, b::Float64)
-        return operator(a, b)
-    end
-end
+# # You can also interpolate variables into the endpoint
+# operations = Dict("add" => +, "multiply" => *)
+# for (pathname, operator) in operations
+#     @get "/$pathname/{a}/{b}" function (req, a::Float64, b::Float64)
+#         return operator(a, b)
+#     end
+# end
 
-# demonstate how to use path params (without type definitions)
-@get "/power/{a}/{b}" function (req::HTTP.Request, a::String, b::String)
-    return parse(Float64, a) ^ parse(Float64, b)
-end
+# # demonstate how to use path params (without type definitions)
+# @get "/power/{a}/{b}" function (req::HTTP.Request, a::String, b::String)
+#     return parse(Float64, a) ^ parse(Float64, b)
+# end
 
-# demonstrate how to use path params with type definitions
-@get "/divide/{a}/{b}" function (req::HTTP.Request, a::Float64, b::Float64)
-    return a / b
-end
+# # demonstrate how to use path params with type definitions
+# @get "/divide/{a}/{b}" function (req::HTTP.Request, a::Float64, b::Float64)
+#     return a / b
+# end
 
-# Return the body of the request as a string
-@post "/echo-text" function (req::HTTP.Request)
-    return text(req)
-end
+# # Return the body of the request as a string
+# @post "/echo-text" function (req::HTTP.Request)
+#     return text(req)
+# end
 
-# demonstrates how to serialize JSON into a julia struct 
-@post "/animal" function (req)
-    return json(req, Animal)
-end
+# # demonstrates how to serialize JSON into a julia struct 
+# @post "/animal" function (req)
+#     return json(req, Animal)
+# end
 
-# Return the body of the request as a JSON object
-@post "/echo-json" function (req::HTTP.Request)
-    return json(req)
-end
+# # Return the body of the request as a JSON object
+# @post "/echo-json" function (req::HTTP.Request)
+#     return json(req)
+# end
 
-# You can also return your own customized HTTP.Response object from an endpoint
-@get "/custom-response" function (req::HTTP.Request)
-    test_value = 77.8
-    return HTTP.Response(200, ["Content-Type" => "text/plain"], body = "$test_value")
-end
+# # You can also return your own customized HTTP.Response object from an endpoint
+# @get "/custom-response" function (req::HTTP.Request)
+#     test_value = 77.8
+#     return HTTP.Response(200, ["Content-Type" => "text/plain"], body = "$test_value")
+# end
 
-# Any object retuned from a function will automatically be converted into JSON (by default)
-@get "/json" function(req::HTTP.Request)
-    return Dict("message" => "hello world", "animal" => Animal(1, "cat", "whiskers"))
-end
+# # Any object retuned from a function will automatically be converted into JSON (by default)
+# @get "/json" function(req::HTTP.Request)
+#     return Dict("message" => "hello world", "animal" => Animal(1, "cat", "whiskers"))
+# end
 
-# show how to return a file from an endpoint
-@get "/files" function (req)
-    return file("main.jl")
-end
+# # show how to return a file from an endpoint
+# @get "/files" function (req)
+#     return file("main.jl")
+# end
 
-# show how to return a string that needs to be interpreted as html
-@get "/string-as-html" function (req)
-    message = "Hello World!"
-    return html("""
-        <!DOCTYPE html>
-            <html>
-            <body> 
-                <h1>$message</h1>
-            </body>
-        </html>
-    """)
-end
+# # show how to return a string that needs to be interpreted as html
+# @get "/string-as-html" function (req)
+#     message = "Hello World!"
+#     return html("""
+#         <!DOCTYPE html>
+#             <html>
+#             <body> 
+#                 <h1>$message</h1>
+#             </body>
+#         </html>
+#     """)
+# end
 
-@route ["GET", "POST"] "/demo" function(req)
-    return Animal(1, "cat", "whiskers")
-end
+# @route ["GET", "POST"] "/demo" function(req)
+#     return Animal(1, "cat", "whiskers")
+# end
 
 # recursively mount all files inside the demo folder ex.) demo/main.jl => /static/demo/main.jl 
-@staticfiles "content"
-@dynamicfiles "content" "dynamic"
+# staticfiles("content", "/")
+
+myvalue = "content"
+@staticfiles(myvalue)
 
 
 # CORS headers that show what kinds of complex requests are allowed to API
-headers = [
-    "Access-Control-Allow-Origin" => "*",
-    "Access-Control-Allow-Headers" => "*",
-    "Access-Control-Allow-Methods" => "GET, POST"
-]
+# headers = [
+#     "Access-Control-Allow-Origin" => "*",
+#     "Access-Control-Allow-Headers" => "*",
+#     "Access-Control-Allow-Methods" => "GET, POST"
+# ]
 
-function CorsHandler(handle)
-    return function(req::HTTP.Request)
-        # return headers on OPTIONS request
-        if HTTP.method(req) == "OPTIONS"
-            return HTTP.Response(200, headers)
-        else 
-            return handle(req)
-        end
-    end
-end
+# function CorsHandler(handle)
+#     return function(req::HTTP.Request)
+#         # return headers on OPTIONS request
+#         if HTTP.method(req) == "OPTIONS"
+#             return HTTP.Response(200, headers)
+#         else 
+#             return handle(req)
+#         end
+#     end
+# end
 
-# start the web server
-serve(middleware=[CorsHandler])
+# # start the web server
+# serve(middleware=[CorsHandler])
+serve()
 
 end
 
--- src/fileutil.jl
+++ src/fileutil.jl
@@ -5,7 +5,7 @@ using HTTP
 include("mimetypes.jl");    
 using .MimeTypes
 
-export file, mountedfolders, @mountfolder
+export file, mountedfolders, mountfolder
 
 """
     file(filepath::String)
@@ -33,17 +33,13 @@ function getfiles(folder::String)
 end
 
 """
-    @iteratefiles(folder::String, func::Function)
+    iteratefiles(func::Function, folder::String)
 
 Walk through all files in a directory and apply a function to each file
 """
-macro iteratefiles(folder::String, func)
-    local target_files::Array{String} = getfiles(folder)
-    quote
-        local action = $(esc(func))
-        for filepath in $target_files
-            action(filepath)
-        end
+function iteratefiles(func::Function, folder::String)
+    for filepath in getfiles(folder)
+        func(filepath)
     end
 end
 
@@ -59,53 +55,47 @@ end
 """
     mountfolder(folder::String, mountdir::String, addroute::Function)
 
-This macro is used to discover files & register them to the router while  
+This function is used to discover files & register them to the router while  
 leaving the `addroute` function to determine how to register the files
 """
-macro mountfolder(folder::String, mountdir::String, addroute)
-    quote 
-        local folder = $folder
-        local directory = $mountdir
-        local separator = Base.Filesystem.path_separator
+function mountfolder(folder::String, mountdir::String, addroute)
+    
+    separator = Base.Filesystem.path_separator
 
-        # track all registered paths 
-        local paths = Dict{String, Bool}()
+    # track all registered paths 
+    paths = Dict{String, Bool}()
 
-        @iteratefiles $folder function(filepath::String)
+    iteratefiles(folder) do filepath
 
-            # remove the first occurrence of the root folder from the filepath before "mounting"
-            local cleanedmountpath = replace(filepath, "$(folder)$(separator)" => "", count=1)
+        # remove the first occurrence of the root folder from the filepath before "mounting"
+        cleanedmountpath = replace(filepath, "$(folder)$(separator)" => "", count=1)
 
-            # generate the path to mount the file to
-            local mountpath = "/$directory/$cleanedmountpath"
+        # generate the path to mount the file to
+        mountpath = mountdir == "/" || isnothing(mountdir) || isempty(mountdir) || all(isspace, mountdir) ? "/$cleanedmountpath" : "/$mountdir/$cleanedmountpath"
 
-            # load file into memory on sever startup
-            local body = file(filepath)
+        # precalculate content type 
+        content_type = mimetype(filepath)
+        headers = ["Content-Type" => content_type]
 
-            # precalculate content type 
-            local content_type = mimetype(filepath) #HTTP.sniff(body)
-            local headers = ["Content-Type" => content_type]
+        paths[mountpath] = true 
+        # register the file route
+        addroute(mountpath, headers, filepath, paths)
 
-            paths[mountpath] = true 
-            # register the file route
-            addroute(mountpath, headers, filepath, paths)
+        # also register file to the root of each subpath if this file is an index.html
+        if endswith(mountpath, "/index.html")
 
-            # also register file to the root of each subpath if this file is an index.html
-            if endswith(mountpath, "/index.html")
+            # # add the route without the trailing "/" character
+            # bare_path = getbefore(mountpath, "/index.html")
+            # paths[bare_path] = true
+            # addroute(bare_path, headers, filepath, paths)
 
-                # # add the route without the trailing "/" character
-                # bare_path = getbefore(mountpath, "/index.html")
-                # paths[bare_path] = true 
-                # addroute(bare_path, headers, filepath, paths)
-
-                # add the route with the trailing "/" character
-                trimmedpath = getbefore(mountpath, "index.html")
-                paths[trimmedpath] = true 
-                addroute(trimmedpath, headers, filepath, paths)
-            
-            end
-
+            # add the route with the trailing "/" character
+            trimmedpath = getbefore(mountpath, "index.html")
+            paths[trimmedpath] = true
+            addroute(trimmedpath, headers, filepath, paths)
+        
         end
+
     end
     
 end

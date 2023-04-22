92c46cdff8789156ec509f712d58100b2f2b07d6
Nathan Ortega <nate.ortega95@gmail.com>
2023-04-22 00:00:30 -0400

    added a error_handling keyword argument which can be use to toggle off the default error handling

---
+++ demo/errorhandling.jl
@@ -0,0 +1,35 @@
+module ErrorHandlingDemo 
+
+include("../src/Oxygen.jl")
+using .Oxygen
+using HTTP
+
+@get "/greet" function(req::HTTP.Request)
+    return "hello world!"
+end
+
+@get "/bad" function(req::HTTP.Request)
+    throw("whoops")
+    "hello"
+end
+
+
+function errorcatcher(handle)
+    function(req)
+        try 
+            println("here!")
+            response = handle(req)
+            println("response: $(String(response.body))")
+            return response
+        catch e 
+            println("whoops there was an error")
+            return HTTP.Response(200, "nice")
+        end
+    end
+end
+
+
+# start the web server
+serveparallel(middleware=[errorcatcher], error_handling=false)
+
+end

16ea7e55f2a03004af44ffdda11e3a6f6d3b4ac0
Nathan Ortega <ndo2439@rit.edu>
2023-06-15 20:29:34 -0400

    bugfix/routing-function-type-support (#116)
    1.) Added support for router() functions when passed to routing functions
    2.) had to remove inline function routing syntax to support the previous feature
    
    * bumped version

--- demo/routingfunctions.jl
+++ demo/routingfunctions.jl
@@ -6,21 +6,39 @@ using .Oxygen
 using HTTP
 
 
-@get "/" function()
+get("/") do
     "hello"
 end
 
-get("/add/{x}/{y}") do request::HTTP.Request, x::Int, y::Int
+math = router("/math", tags=["math"])
+
+get(math("/add/{x}/{y}")) do request::HTTP.Request, x::Int, y::Int
     x + y
 end
 
-get("/multiply/{x}/{y}", function(request::HTTP.Request, x::Int, y::Int)
-    x * y
-end)
+route(["POST"], math("/other/{x}/{y}")) do req, x::Int, y::Int
+    x - y 
+end
 
-put("/put", () -> "put")
-patch("/patch", () -> "patch")
-delete("/delete", () -> "delete")
+get(math("/multiply/{x}/{y}")) do request::HTTP.Request, x::Int, y::Int
+    x * y
+end
+
+get("/get") do 
+    "test"
+end
+
+put("/put") do 
+    "put" 
+end
+
+patch("/patch") do 
+    "patch" 
+end
+
+delete("/delete") do 
+    "delete" 
+end
 
 # start the web server
 serve()

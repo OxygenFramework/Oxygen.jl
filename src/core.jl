77a19ddef27d44202cf2eac7dd090de92f5e6b2b
Nathan Ortega <ndo2439@rit.edu>
2023-09-27 23:21:26 -0400

    feature/package-compiler-compatibility (#128)
    1.) added RelocatableFolders to make sure this package works with PackageCompiler.jl
    2.) Now spawning cron tasks in any available thread of just the main thread
    3.) added redoc docs option, but still default to swagger
    4.) bumped version to 1.2

--- src/core.jl
+++ src/core.jl
@@ -570,7 +570,15 @@ function setupswagger()
         return swaggerhtml()
     end
 
-    @get "$schemapath" function()
+    @get "$docspath/swagger" function()
+        return swaggerhtml()
+    end
+
+    @get "$docspath/redoc" function()
+        return redochtml()
+    end
+
+    @get "$(getschemapath())" function()
         return getschema() 
     end
     

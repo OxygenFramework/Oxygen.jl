e35eb2cc4600e8dd36050d476b944d0c216303ae
Nathan Ortega <nate.ortega95@gmail.com>
2024-01-12 20:14:14 -0500

    updated requires section

--- src/extensions/load.jl
+++ src/extensions/load.jl
@@ -1,18 +1,11 @@
 using Requires
 
-################################################################
-#                       Templating Extensions                     #
-################################################################
 function __init__()
 
-    @require Mustache="ffc61752-8dc7-55ee-8c37-f3e9cdd09e70" begin
-        include("templating/mustache.jl"); using .MustacheTemplating
-        export mustache
-    end
-
-    @require OteraEngine="b2d7f28f-acd6-4007-8b26-bc27716e5513" begin
-        include("templating/oteraengine.jl"); using .OteraEngineTemplating
-        export otera
-    end
+    ################################################################
+    #                       Templating Extensions                  #
+    ################################################################
+    @require Mustache="ffc61752-8dc7-55ee-8c37-f3e9cdd09e70" include("templating/mustache.jl")
+    @require OteraEngine="b2d7f28f-acd6-4007-8b26-bc27716e5513" include("templating/oteraengine.jl")
 
 end

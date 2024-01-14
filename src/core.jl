cc8a08e046bc1baa487aae876026142e09cc508d
Nathan Ortega <nate.ortega95@gmail.com>
2024-01-13 22:37:39 -0500

    bumped version

--- src/core.jl
+++ src/core.jl
@@ -42,7 +42,7 @@ oxygen_title = raw"""
 
 function serverwelcome(host::String, port::Int, docs::Bool, metrics::Bool)
     printstyled(oxygen_title, color = :blue, bold = true)
-    @info "📦 Version 1.4.0 (2024-01-12)"
+    @info "📦 Version 1.4.1 (2024-01-13)"
     @info "✅ Started server: http://$host:$port" 
     docs    && @info "📖 Documentation: http://$host:$port$docspath"
     metrics && @info "📊 Metrics: http://$host:$port$docspath/metrics"

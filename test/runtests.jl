5cc35f41826cb0cd617f076afcb760b1f48cd5ed
Nathan Ortega <nate.ortega95@gmail.com>
2024-03-30 19:45:17 -0400

    uncommented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,30 +4,30 @@ include("constants.jl"); using .Constants
 
 #### Extension Tests ####
 
-# include("extensions/templatingtests.jl")
-# include("extensions/cairomakietests.jl")
-# include("extensions/wglmakietests.jl")
-# include("extensions/bonitotests.jl")
-# include("extensions/protobuf/protobuftests.jl")
+include("extensions/templatingtests.jl")
+include("extensions/cairomakietests.jl")
+include("extensions/wglmakietests.jl")
+include("extensions/bonitotests.jl")
+include("extensions/protobuf/protobuftests.jl")
 
 #### Sepcial Handler Tests ####
 
 include("ssetests.jl")
-# include("websockettests.jl")
-# include("streamingtests.jl")
+include("websockettests.jl")
+include("streamingtests.jl")
 
 #### Core Tests ####
 
-# include("metricstests.jl")
-# include("routingfunctionstests.jl")
-# include("rendertests.jl")
-# include("bodyparsertests.jl")
-# include("crontests.jl")
-# include("oxidise.jl")
-# include("instancetests.jl")
-# include("paralleltests.jl")
-# include("taskmanagement.jl")
-# include("cronmanagement.jl")
-# include("originaltests.jl")
+include("metricstests.jl")
+include("routingfunctionstests.jl")
+include("rendertests.jl")
+include("bodyparsertests.jl")
+include("crontests.jl")
+include("oxidise.jl")
+include("instancetests.jl")
+include("paralleltests.jl")
+include("taskmanagement.jl")
+include("cronmanagement.jl")
+include("originaltests.jl")
 
 end 

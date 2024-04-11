5f3efdf5527f2000cb32cc9243cb72b970e352d9
Nathan Ortega <ndo2439@rit.edu>
2024-04-11 19:22:39 -0400

    bugfix/relocatability-fix (#185)
    added a new PACKAGE_DIR const using the @path macro from RelocatableFolders

--- src/constants.jl
+++ src/constants.jl
@@ -2,15 +2,18 @@ module Constants
 using HTTP
 using RelocatableFolders
 
-export DATA_PATH,
+export PACKAGE_DIR, DATA_PATH,
     GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, CONNECT, TRACE, 
     HTTP_METHODS,
     WEBSOCKET, STREAM,
     SPECIAL_METHODS, METHOD_ALIASES, TYPE_ALIASES
 
+# Generate a reliable path to our package directory
+const PACKAGE_DIR = @path @__DIR__
+
 # Generate a reliable path to our internal data folder that works when the 
 # package is used with PackageCompiler.jl
-const DATA_PATH = @path abspath(joinpath(@__DIR__, "..", "data"))
+const DATA_PATH = @path joinpath(@__DIR__, "..", "data")
 
 # HTTP Methods
 const GET       = "GET"

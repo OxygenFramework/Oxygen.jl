4b56baf86556c53c65ab8f50f319b53d75565ae5
Nathan Ortega <ndo2439@rit.edu>
2024-09-02 11:56:33 -0400

    feature/interactive-threadpool-warning (#213)
    added warning log that checks for empty interactive threadpools when running in parallel mode

--- src/core.jl
+++ src/core.jl
@@ -10,7 +10,7 @@ using Dates
 using Reexport
 using RelocatableFolders
 using DataStructures: CircularDeque
-import Base.Threads: lock
+import Base.Threads: lock, nthreads
 
 include("util.jl");         @reexport using .Util
 include("types.jl");        @reexport using .Types 
@@ -51,6 +51,16 @@ function serverwelcome(host::String, port::Int, docs::Bool, metrics::Bool, paral
     end
     if parallel
         @info "🚀 Running in parallel mode with $(Threads.nthreads()) threads"
+        # Check if the current julia version supports interactive threadpools
+        if hasmethod(getfield(Threads, Symbol("@spawn")), Tuple{LineNumberNode, Module, Symbol, Expr})
+            # Add a warnign if the interactive threadpool is empty when running in parallel mode
+            if nthreads(:interactive) == 0
+                @warn """
+                🚨 Interactive threadpool is empty. This can hurt performance when running in parallel mode.
+                Try launching julia like \"julia --threads 3,1\" to add 1 thread to the interactive threadpool.
+                """
+            end
+        end
     end
 end
 

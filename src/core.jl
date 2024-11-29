ab2fa02618ca55a7ce0d513f1ee17a87f787ff21
Nathan Ortega <nate.ortega95@gmail.com>
2024-11-28 21:09:34 -0500

    bumped version

--- src/core.jl
+++ src/core.jl
@@ -43,7 +43,7 @@ oxygen_title = raw"""
 
 function serverwelcome(external_url::String, docs::Bool, metrics::Bool, parallel::Bool, docspath::String)
     printstyled(oxygen_title, color=:blue, bold=true)
-    @info "📦 Version 1.5.14 (2024-10-17)"
+    @info "📦 Version 1.5.15 (2024-11-28)"
     @info "✅ Started server: $external_url"
     if docs
         @info "📖 Documentation: $external_url" * docspath

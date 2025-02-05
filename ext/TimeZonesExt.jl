6b154432c823c545aa3d19c126a6142aecb4d8af
Nathan Ortega <nate.ortega95@gmail.com>
2025-02-05 17:07:27 -0500

    1.) TimeZonesExt now hooks into autodoc module to handle ZonedDateTime 2.) added hints in the docs when using native DateTime objects 3.) changed example_datetime() to only default to the current year (it was deployed) so we don't expose the exact server start time 4.) added schema and serialization tests to the timezone testset

--- ext/TimeZonesExt.jl
+++ ext/TimeZonesExt.jl
@@ -1,16 +1,27 @@
 module TimeZonesExt
 
-import Base
 import HTTP
 import TimeZones: ZonedDateTime, ISOZonedDateTimeFormat
-import Oxygen
+import Oxygen.Core.Types: Nullable
+import Oxygen.Core.Util: parseparam
+import Oxygen.Core.AutoDoc: is_custom_struct, gettype, getformat
 
-function Base.parse(::Type{ZonedDateTime}, str::String)
-    return ZonedDateTime(str, ISOZonedDateTimeFormat)
-end
+export parseparam, is_custom_struct, gettype, getformat
 
-function Oxygen.parseparam(::Type{T}, str::String; escape=true) where {T <: ZonedDateTime}
+####################################
+# Util parsing overloads           #
+####################################
+
+function parseparam(::Type{T}, str::String; escape=true) where {T <: ZonedDateTime}
     return parse(T, escape ? HTTP.unescapeuri(str) : str)
 end
+
+####################################
+# AutoDoc Overloads                #
+####################################
+
+is_custom_struct(::Type{ZonedDateTime}) :: Bool = false
+gettype(::Type{ZonedDateTime}) :: String = "string"
+getformat(::Type{ZonedDateTime}) :: Nullable{String} = "date-time"
 
 end

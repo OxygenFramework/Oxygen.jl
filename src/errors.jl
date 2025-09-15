857836b1d2cfeb9c7d426049c51987dff6456f40
Nathan Ortega <nate.ortega95@gmail.com>
2025-09-15 00:25:03 -0400

    Added validation updates from @Euraxluo 's PR

--- src/errors.jl
+++ src/errors.jl
@@ -6,10 +6,17 @@ export ValidationError
 # This is used by the Extractors.jl module to signal that a validation error has occurred
 struct ValidationError <: Exception
     msg::String
+    cause::Union{Nothing, Exception}
+    ValidationError(msg::String) = new(msg, nothing)
+    ValidationError(msg::String, cause::Exception) = new(msg, cause)
 end
 
 function Base.showerror(io::IO, e::ValidationError)
     print(io, "Validation Error: $(e.msg)")
+    if !isnothing(e.cause)
+        print(io, "\nCaused by: ")
+        showerror(io, e.cause)
+    end
 end
 
 end

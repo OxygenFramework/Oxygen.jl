b6dfe4fde146e089f60c1f2b17c340bfff464784
Nathan Ortega <nate.ortega95@gmail.com>
2026-02-24 22:32:18 -0500

    bumped version

--- src/core.jl
+++ src/core.jl
@@ -44,7 +44,7 @@ oxygen_title = raw"""
 function serverwelcome(external_url::String, prefix::Nullable{String}, docs::Bool, metrics::Bool, parallel::Bool, docspath::String)
     printstyled(oxygen_title, color=:blue, bold=true)
     server_url = join_url_path(external_url, prefix)
-    @info "📦 Version 1.10.0 (2026-01-01)"
+    @info "📦 Version 1.10.1 (2026-01-01)"
     if !isnothing(prefix)
         @info "🏷️  Global path prefix: $prefix"
     end

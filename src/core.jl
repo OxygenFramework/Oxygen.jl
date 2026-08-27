3ae545bb1a418aeaeece9d242ec314354a6133f5
Nathan Ortega <nate.ortega95@gmail.com>
2026-08-26 23:48:30 -0400

    1.) added oxygen_logfmt to module exports 2.) reverted the premature release date in core

--- src/core.jl
+++ src/core.jl
@@ -50,7 +50,7 @@ oxygen_title = raw"""
 function serverwelcome(external_url::String, prefix::Nullable{String}, docs::Bool, metrics::Bool, parallel::Bool, docspath::String)
     printstyled(oxygen_title, color=:blue, bold=true)
     server_url = join_url_path(external_url, prefix)
-    @info "📦 Version 1.11.2 (2026-08-26)"
+    @info "📦 Version 1.10.2 (2026-04-18)"
     if !isnothing(prefix)
         @info "🏷️  Global path prefix: $prefix"
     end
